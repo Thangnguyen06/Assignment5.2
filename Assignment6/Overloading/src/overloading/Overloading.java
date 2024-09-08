6d550f38b336f9f1d0e5ad5ea7b488e0a544d396
Nguyenthang <150777483+Thangnguyen06@users.noreply.github.com>
2024-09-08 22:29:57 +0700

    Add files via upload

---
+++ Assignment6/Overloading/src/overloading/Overloading.java
@@ -0,0 +1,20 @@
+
+package overloading;
+
+class Calculation2{
+    void sum(int a, int b){System.out.println(a+b);}
+    void sum(double a, double b){System.out.println(a+b);}
+
+ }
+ public class Overloading{
+
+  
+    public static void main(String[] args) 
+    {
+        Calculation2 obj=new Calculation2();
+        obj.sum(10.5,10.5);
+        obj.sum(20,20);    
+    }
+    }
+    
+
